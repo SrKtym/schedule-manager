0d265aa4d6a90ec5a8c21264282590449187b1a6
SrKtym <frmywrkplc@gmail.com>
2025-09-22 17:05:46 +0900

    refactor: 微修正

--- app/home/schedule/page.tsx
+++ app/home/schedule/page.tsx
@@ -1,13 +1,28 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
+import { Overall } from "@/components/home/schedule/overall";
+import { fetchRegisteredCourseData, fetchSchedule, getSession } from "@/utils/fetch";
+import { ScheduleProvider } from "@/contexts/schedule-context";
+
 
 export const metadata: Metadata = {
-    title: '時間割'
+    title: 'スケジュール'
 }
 
-export default function Page() {
+export default async function SchedulePage() {
+    const session = await getSession();
+    const [registeredCourseData, schedule] = await Promise.allSettled([
+        fetchRegisteredCourseData(session),
+        fetchSchedule(session)
+    ]);
+    const registeredCourseValue = registeredCourseData.status === 'fulfilled' ? registeredCourseData.value : [];
+    const scheduleValue = schedule.status === 'fulfilled' ? schedule.value : [];
+
     return (
-        <div>
-            
-        </div>
+        <ScheduleProvider 
+            registeredCourse={registeredCourseValue}
+            schedule={scheduleValue}
+        >
+            <Overall />
+        </ScheduleProvider>
     );
 }
