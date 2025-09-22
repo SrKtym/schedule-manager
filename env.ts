ab22234dc19852f601e5dbd462ed8b7d8aaee7af
SrKtym <frmywrkplc@gmail.com>
2025-09-22 19:48:20 +0900

    chore: admin_user_idの追加

--- env.ts
+++ env.ts
@@ -1,5 +1,5 @@
 import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
+import { z } from 'zod/v4';
 
 
 export const env = createEnv({
@@ -13,12 +13,13 @@ export const env = createEnv({
         TWITTER_CLIENT_SECRET: z.string(),
         RESEND_API_KEY: z.string(),
         ARCJET_KEY: z.string(),
-        DATABASE_URL: z.string().url()
+        DATABASE_URL: z.url(),
+        BETTER_AUTH_ADMIN_USER_ID: z.string(),
     },
 
     client: {
-        NEXT_PUBLIC_APP_URL: z.string().url(),
-        NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+        NEXT_PUBLIC_APP_URL: z.url(),
+        NEXT_PUBLIC_SUPABASE_URL: z.url(),
         NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string()
     },
 
@@ -35,6 +36,7 @@ export const env = createEnv({
         ARCJET_KEY: process.env.ARCJET_KEY!,
         DATABASE_URL: process.env.DATABASE_URL!,
         NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        BETTER_AUTH_ADMIN_USER_ID: process.env.BETTER_AUTH_ADMIN_USER_ID!
     }
 });
