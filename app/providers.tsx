137c1caf1789bd9ae2c642b6a7d0cf4e04e62db0
SrKtym <frmywrkplc@gmail.com>
2025-09-22 17:25:56 +0900

    refactor: 微修正

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,7 @@ import { HeroUIProvider, ToastProvider } from "@heroui/react";
 
 export function Providers({children}: {children: React.ReactNode}) {
     return (
-        <HeroUIProvider>
+        <HeroUIProvider locale="ja-JP">
             <ToastProvider placement="top-center"/>
             {children}
         </HeroUIProvider>
