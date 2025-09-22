9075844c901a4bac053f42e5b4a59b9b6a6d91fb
SrKtym <frmywrkplc@gmail.com>
2025-09-22 17:13:56 +0900

    refactor: 微修正

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,16 +1,15 @@
 import 'dotenv/config';
 import { db } from "@/lib/drizzle";
 import { course } from '@/lib/drizzle/schema/public';
-import { courseList } from '@/lib/data';
+import { courseList } from '@/constants/course-data';
 
 
 export async function GET() {
     const res = await db.transaction(async (tx) => {
         try {
             await tx.insert(course).values(courseList.map((value) => {return value})).onConflictDoNothing();
-            return Response.json({message: 'Database seeded successfully' });
+            return Response.json({ message: 'Database seeded successfully' });
         } catch (error) {
-            tx.rollback();
             return Response.json({ error }, { status: 500 });
         }
     });
