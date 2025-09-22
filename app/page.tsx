6376b8514571982f232694bb0350f1aa05e3deae
SrKtym <frmywrkplc@gmail.com>
2025-09-22 17:25:04 +0900

    style: ランディングページ風にスタイリング

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
-import { Button } from '@heroui/react';
-import Link from "next/link";
+import { FeaturesSection } from '@/components/landing/features-section';
+import { HeroSection } from '@/components/landing/hero-section';
 
-export default function StartPage() {
+export default function LandingPage() {
     return (
-        <main className="flex items-center justify-center h-screen bg-gradient-to-r from-purple-400 to-pink-400">
-            <div className="text-center text-white drop-shadow-xl space-y-8 mx-3" >
-                <h1 className="font-semibold text-3xl sm:text-6xl">🔐認証チュートリアル</h1>
-                <p>Nextjs(App Router), TypeScript, Supabaseを用いた認証機能の実装</p>
-                <Link href='/sign-in'>
-                    <Button color='primary'>はじめる</Button>
-                </Link>
-            </div>
+        <main className='min-h-screen'>
+            <HeroSection />
+            <FeaturesSection />
         </main>
     );
 }
