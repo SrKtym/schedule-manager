da825f2fa461de44782211fe9c7bba985656837e
SrKtym <frmywrkplc@gmail.com>
2025-09-22 16:13:31 +0900

    refactor: 微修正

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 export default function AuthLayout({children}: {children: React.ReactNode}) {
     return (
         <main className='flex items-center justify-center min-h-screen bg-gradient-to-b from-purple-200 to-gray-200'>
-            <div className="w-full max-w-[480px] m-auto p-4">
+            <div className="w-full max-w-[480px] p-4">
                 {children}
             </div>
         </main>
