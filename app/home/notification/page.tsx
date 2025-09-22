3b0ecefe26a512d2ba9e500e1f9efe5056721c37
SrKtym <frmywrkplc@gmail.com>
2025-09-22 17:00:36 +0900

    refactor: 微修正

--- app/home/notification/page.tsx
+++ app/home/notification/page.tsx
@@ -1,13 +1,28 @@
-import { Metadata } from "next";
-import { getSession } from "@/lib/fetch";
-import { EmailInbox } from "@/components/private/notification/inbox";
-
+import type { Metadata } from "next";
+import { fetchMessages, getSession } from "@/utils/fetch";
+import { Inbox } from "@/components/home/notification/inbox";
+import { Suspense } from "react";
+import { InboxSkelton } from "@/components/skeltons";
+import { MessagesProvider } from "@/contexts/messages-context";
 
 export const metadata: Metadata = {
     title: '通知'
 }
 
 export default async function NotificationPage() {
-    const session = await getSession();
-    if (session) return <EmailInbox email={session.user.email}/>
+    const session = await getSession()
+    const data = await fetchMessages(session);
+    
+    if (session) {
+        return (
+            <Suspense fallback={<InboxSkelton />}>
+                <MessagesProvider 
+                    email={session.user.email} 
+                    messageList={data}
+                >
+                    <Inbox />
+                </MessagesProvider>
+            </Suspense>
+        );
+    }
 }
