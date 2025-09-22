36892c1129f858ab2887c534551d07f293722054
SrKtym <frmywrkplc@gmail.com>
2025-09-22 19:19:57 +0900

    refactor: 微修正

---
+++ public/fonts/fonts.ts
@@ -0,0 +1,11 @@
+import { Noto_Sans_JP, Noto_Serif_JP } from 'next/font/google';
+
+export const notoSansJP = Noto_Sans_JP({
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+export const notoSerifJP = Noto_Serif_JP({
+    subsets: ['latin'],
+    display: 'swap',
+});
