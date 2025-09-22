1af9e1d6959637989b5d6ad0af0a8c9f620dc12e
SrKtym <frmywrkplc@gmail.com>
2025-09-22 19:18:07 +0900

    fix: 微修正

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -4,4 +4,10 @@ import { env } from '@/env'
 const supabaseUrl = env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
+export const supabase = createBrowserClient(supabaseUrl, supabaseKey, {
+    // global: {
+    //     headers: {
+    //         Authorization: `Bearer ${jwt}`
+    //     }
+    // }
+});
