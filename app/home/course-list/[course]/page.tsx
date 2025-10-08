3bce7353dc8e8819fb939183a926424d2287a830
SrKtym <frmywrkplc@gmail.com>
2025-10-08 17:11:32 +0900

    refactor: 微修正

--- app/home/course-list/[course]/page.tsx
+++ app/home/course-list/[course]/page.tsx
@@ -1,12 +1,9 @@
 import { Overall } from "@/components/home/course-list/course/overall";
-import { RegisteredCourseProvider } from "@/contexts/registered-course-context";
 import { 
-    getSession, 
-    fetchRegisteredCourseData, 
     fetchAssignment, 
     fetchAnnouncement,
     fetchMemberList 
-} from "@/utils/fetch";
+} from "@/utils/getter";
 import { AssignmentDataProvider } from "@/contexts/assignment-data-context";
 import { AnnouncementProvider } from "@/contexts/announcement-context";
 import { MemberProvider } from "@/contexts/member-context";
@@ -16,33 +13,25 @@ export default async function CoursePage(props: {
         course: string
     }>
 }) {
-    const session = await getSession();
     const paramsValue = await props.params;
     const courseName = paramsValue.course;
     const decodedCourseName = decodeURIComponent(courseName);
-    const [courseDataList, assignment, announcement, memberList] = await Promise.allSettled([
-        fetchRegisteredCourseData(session),
+    const [assignment, announcement, memberList] = await Promise.allSettled([
         fetchAssignment(decodedCourseName),
         fetchAnnouncement(decodedCourseName),
         fetchMemberList(decodedCourseName)
     ]);
-    const courseDataListValue = courseDataList.status === 'fulfilled' ? courseDataList.value : [];
     const assignmentValue = assignment.status === 'fulfilled' ? assignment.value : null;
     const announcementValue = announcement.status === 'fulfilled' ? announcement.value : [];
     const memberListValue = memberList.status === 'fulfilled' ? memberList.value : [];
 
     return (
-        <RegisteredCourseProvider
-            courseDataList={courseDataListValue} 
-            courseName={decodedCourseName}
-        >
-            <AssignmentDataProvider assignmentData={assignmentValue}>
-                <AnnouncementProvider announcement={announcementValue}>
-                    <MemberProvider memberList={memberListValue}>
-                        <Overall />
-                    </MemberProvider>
-                </AnnouncementProvider>
-            </AssignmentDataProvider>
-        </RegisteredCourseProvider>
+        <AssignmentDataProvider assignmentData={assignmentValue}>
+            <AnnouncementProvider announcement={announcementValue}>
+                <MemberProvider memberList={memberListValue}>
+                    <Overall courseName={decodedCourseName} />
+                </MemberProvider>
+            </AnnouncementProvider>
+        </AssignmentDataProvider>
     );
 }
