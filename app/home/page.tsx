e3e5045802cb352dfbbed686f2d2303c591fa124
SrKtym <frmywrkplc@gmail.com>
2025-10-08 17:26:26 +0900

    feat: ホームページにコンポーネントを複数追加

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,28 @@
 import type { Metadata } from "next";
+import { NotificationsList } from "@/components/home/notifications-list";
+import { DailySchedule } from "@/components/home/daily-schedule";
+import { UpcomingAssignment } from "@/components/home/upcoming-assignment";
+import { AssignmentProgress } from "@/components/home/assignment-progress";
+import { getSession } from "@/utils/getter";
+import { fetchSchedule } from "@/utils/getter";
 
 export const metadata: Metadata = {
     title: 'ホーム'
 }
 
-export default function HomePage() {
-    return;
+export default async function HomePage() {
+    const session = await getSession();
+    const schedule = await fetchSchedule(session) ?? [];
+    const todaySchedule = schedule.filter(
+        schedule => schedule.start.toDateString() === new Date().toDateString()
+    );
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-flow-col grid-rows-3 auto-cols-fr gap-6 p-3">
+            <DailySchedule todaySchedule={todaySchedule} />
+            <NotificationsList />
+            <UpcomingAssignment />
+            <AssignmentProgress />
+        </div>
+    );
 }
