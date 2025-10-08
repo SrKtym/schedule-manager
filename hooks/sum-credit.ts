d13bf6de996d1a59212191e6466016ffdaa70a93
SrKtym <frmywrkplc@gmail.com>
2025-10-08 18:24:16 +0900

    refactor: 微修正

--- hooks/sum-credit.ts
+++ hooks/sum-credit.ts
@@ -1,10 +1,12 @@
 'use client'
 
-import { useRegisteredCourseData } from "@/contexts/schedule-context";
+import { useRegisteredCourseData } from "@/contexts/registered-course-context";
 
 export function sumCredit() {
     const dataList = useRegisteredCourseData();
-    const creditList = dataList.map((data) => {return Number(data.course.credit)});
+    const creditList = dataList.courseDataList.map((data) => {
+        return Number(data.course.credit)
+    });
     const sum = creditList.reduce((prev, curr) => prev + curr, 0);
     return sum;
 }
