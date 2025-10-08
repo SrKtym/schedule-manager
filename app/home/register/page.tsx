54a291ef0b66a8eb818b4aeddb1d2954bc177a9d
SrKtym <frmywrkplc@gmail.com>
2025-10-08 17:16:08 +0900

    refactor: 微修正

--- app/home/register/page.tsx
+++ app/home/register/page.tsx
@@ -11,11 +11,9 @@ import {
 } from "@/constants/definitions";
 import { targetDepartment } from "@/utils/related-to-register";
 import { 
-    fetchRegisteredCourseData,
     getCourse, 
-    getItemsLength,  
-    getSession 
-} from "@/utils/fetch";
+    getItemsLength
+} from "@/utils/getter";
 import { Timetable } from "@/components/home/register/timetable";
 import type { Metadata } from "next";
 import { 
@@ -56,8 +54,7 @@ export default async function RegisterPage(
     const requiredList = params.required?.split(',') ?? [...required];
     const page = Number(params.page);
     const rows = Number(params.rows);
-    const session = await getSession();
-    const [course, itemsLength, registeredCourseData] = await Promise.allSettled([
+    const [course, itemsLength] = await Promise.allSettled([
         getCourse(
             gradeList, 
             facultyList, 
@@ -79,13 +76,11 @@ export default async function RegisterPage(
             creditList,
             requiredList,
             params.query
-        ),
-        fetchRegisteredCourseData(session)
+        )
     ]);
 
     const courseValue = course.status === 'fulfilled' ? course.value : [];
     const itemsLengthValue = itemsLength.status === 'fulfilled' ? itemsLength.value : 0;
-    const registeredCourseValue = registeredCourseData.status === 'fulfilled' ? registeredCourseData.value : [];
     const totalPages = Math.ceil(itemsLengthValue / (rows || 10));
 
     return (
@@ -106,7 +101,7 @@ export default async function RegisterPage(
             </div>
             <div>
                 <Suspense fallback={<TimeTableSkelton />}>
-                    <ScheduleProvider registeredCourse={registeredCourseValue}>
+                    <ScheduleProvider>
                         <Timetable />
                     </ScheduleProvider>
                 </Suspense>
