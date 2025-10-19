4b84427d347bdf93644a8fcc23bab903b5f3706c
SrKtym <frmywrkplc@gmail.com>
2025-10-19 13:20:27 +0900

    feat: 通知、スケジュールの詳細を表示するモーダルを追加

--- components/home/shared-layout-modal.tsx
+++ components/home/shared-layout-modal.tsx
@@ -2,29 +2,56 @@
 
 import { fetchSchedule } from "@/utils/getter";
 import * as m from "motion/react-m";
+import { X } from "lucide-react";
+import { addToast, Button } from "@heroui/react";
+import { dateOptionforSchedule } from "@/constants/definitions";
+import { isSpanningDays } from "@/utils/related-to-schedule";
+import { hc } from "hono/client";
+import { env } from "@/env";
+import { AppType } from "@/app/api/[[...route]]/route";
+import { useRouter } from "next/navigation";
 
+const CloseButton = ({ onClose }: { onClose: () => void }) => {
+    return (
+        <Button
+            aria-label="close modal"
+            isIconOnly
+            variant="light"
+            radius="full"
+            className="absolute top-0 right-2"
+            onPress={onClose}
+        >
+            <X 
+                width={24} 
+                height={24}
+            />
+        </Button>
+    )
+}
+
+// 通知、講義、スケジュールのモーダル
 export function SingleItemModal({
     notification,
     registeredCourse,
     schedule,
-    onClick
+    onClose,
 }: {
     notification?: { id: number, title: string, message: string, read: boolean };
     registeredCourse?: { day: string, start: Date, end: Date, name: string };
     schedule?: Awaited<ReturnType<typeof fetchSchedule>>[number];
-    onClick: () => void;
+    onClose: () => void;
 }) {
+    const client = hc<AppType>(env.NEXT_PUBLIC_APP_URL);
+    const router = useRouter();
+
     return (
-        <div 
-            className="absolute fixed inset-0 flex items-center justify-center z-30"
-            onClick={onClick}
-        >
+        <div className="absolute fixed inset-0 flex items-center justify-center z-30">
             {/* 通知 */}
             {notification && 
                 <m.div
                     layoutId={notification.id.toString()}
                     key="modal"
-                    className="flex flex-col items-start gap-3 text-justify w-full h-full max-w-[500px] max-h-[300px] rounded-3xl bg-background p-6"
+                    className="relative flex flex-col items-start gap-3 text-justify w-full h-full max-w-[500px] max-h-[300px] rounded-3xl bg-background p-6"
                 >
                     <h2 className="text-lg font-medium">
                         {notification.title}
@@ -32,6 +59,7 @@ export function SingleItemModal({
                     <p className="text-sm">
                         {notification.message}
                     </p>
+                    <CloseButton onClose={onClose} />
                 </m.div>
             }
             {/* 講義 */}
@@ -39,7 +67,7 @@ export function SingleItemModal({
                 <m.div
                     layoutId={registeredCourse.name.toString()}
                     key="modal"
-                    className="flex flex-col items-start gap-3 text-justify w-full h-full max-w-[500px] max-h-[300px] rounded-3xl bg-primary p-6"
+                    className="relative flex flex-col items-start gap-3 text-justify w-full h-full max-w-[500px] max-h-[300px] rounded-3xl bg-primary p-6"
                 >
                     <h2 className="text-lg font-medium">
                         {registeredCourse.name}
@@ -48,28 +76,53 @@ export function SingleItemModal({
                         {registeredCourse.start.toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' })} 
                         - {registeredCourse.end.toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' })}
                     </p>
+                    <CloseButton onClose={onClose} />
                 </m.div>
             }
             {/* スケジュール */}
             {schedule && 
                 <m.div
+                    layout
                     layoutId={schedule.id.toString()}
                     key="modal"
                     style={{
                         backgroundColor: schedule.color
                     }}
-                    className="flex flex-col items-start gap-3 text-justify w-full h-full max-w-[500px] max-h-[300px] rounded-3xl p-6"
+                    className="relative flex flex-col items-start gap-3 text-justify w-full h-full max-w-[500px] max-h-[300px] rounded-3xl p-6"
                 >
                     <h2 className="text-lg font-medium">
                         {schedule.title}
                     </h2>
                     <p className="text-sm">
+                        {isSpanningDays(schedule.start, schedule.end) ? 
+                            `${schedule.start.toLocaleDateString('default', dateOptionforSchedule)} - ${schedule.end.toLocaleDateString('default', dateOptionforSchedule)}` : 
+                            `${schedule.start.toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' })} - ${schedule.end.toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' })}`}
+                    </p>
+                    <p>
                         {schedule.description}
                     </p>
-                    <p className="text-sm">
-                        {schedule.start.toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' })} 
-                        - {schedule.end.toLocaleTimeString('default', { hour: '2-digit', minute: '2-digit' })}
-                    </p>
+                    <CloseButton onClose={onClose} />
+                    <Button
+                        color="danger"
+                        onPress={async () => {
+                            const res = await client.api.schedule.$delete({
+                                json: {
+                                    id: schedule.id
+                                }
+                            });
+                            if (res.ok) {
+                                router.refresh();
+                                onClose();
+                            } else {
+                                addToast({
+                                    color: 'danger',
+                                    description: '予定の削除に失敗しました。'
+                                });
+                            }
+                        }}
+                    >
+                        この予定を削除する
+                    </Button>
                 </m.div>
             }
         </div>
