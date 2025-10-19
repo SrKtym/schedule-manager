9bed301a891303385fc5205bd9a8fbf8b2f8d8f2
SrKtym <frmywrkplc@gmail.com>
2025-10-19 12:53:20 +0900

    refactor: 課題とアナウンスメントを保持するコンテキストを追加

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,35 +1,56 @@
-import { getSession } from "@/utils/getter";
-import { getThemeCookie } from "@/utils/getter";
+import { 
+    fetchRegisteredCourseData, 
+    fetchAssignmentData, 
+    fetchAnnouncement, 
+    fetchSession, 
+    fetchThemeCookie 
+} from "@/utils/getter";
 import { CustomNavbar } from "@/components/home/navbar";
 import { ThemeProvider } from "@/contexts/theme-context";
 import { UserDataProvider, UserData } from "@/contexts/user-data-context";
 import { RegisteredCourseProvider } from "@/contexts/registered-course-context";
-import { fetchRegisteredCourseData } from "@/utils/getter";
-
+import { AssignmentDataProvider } from "@/contexts/assignment-data-context";
+import { AnnouncementProvider } from "@/contexts/announcement-context";
+import { BackToTopButton } from "@/components/home/back-to-top-button";
 
 export default async function HomeLayout({children}: {children: React.ReactNode}) {
-    const session = await getSession();
+    const session = await fetchSession();
     
     if (session) {
         const userData: UserData = session.user;
-        const theme = await getThemeCookie();
-        const registeredCourseData = await fetchRegisteredCourseData(session);
+        const courseDataList = await fetchRegisteredCourseData(session);
+        const courseNameList = courseDataList.map(({course}) => course.name);
+        const [theme, assignmentData, announcementData] = await Promise.allSettled([
+            fetchThemeCookie(),
+            fetchAssignmentData(courseNameList),
+            fetchAnnouncement(courseNameList)
+        ]);
+        const themeValue = theme.status === 'fulfilled' ? theme.value : 'light';
+        const assignmentDataValue = assignmentData.status === 'fulfilled' ? assignmentData.value : null;
+        const announcementDataValue = announcementData.status === 'fulfilled' ? announcementData.value : [];
 
         return (
             <UserDataProvider userData={userData}>
-                <ThemeProvider theme={theme}>
+                <ThemeProvider theme={themeValue}>
                     <CustomNavbar
                         name={session.user.name} 
                         image={session.user.image} 
                     />
                 </ThemeProvider>
-                <RegisteredCourseProvider courseDataList={registeredCourseData}>
-                    <main className="w-full">
-                        {children}
-                    </main>
+                <RegisteredCourseProvider courseDataList={courseDataList}>
+                    <AssignmentDataProvider assignmentData={assignmentDataValue}>
+                        <AnnouncementProvider announcement={announcementDataValue}>
+                            <main className="w-full min-h-screen">
+                                {children}
+                            </main>
+                        </AnnouncementProvider>
+                    </AssignmentDataProvider>
                 </RegisteredCourseProvider>
-                <footer className="flex justify-center w-full px-3 py-5">
-                    フッター
+                <footer className="flex flex-col items-center justify-center pt-5">
+                    <BackToTopButton />
+                    <p className="w-full text-center text-tiny text-white bg-gray-700 px-3 py-5">
+                        Copyright © 2025 Schedule Manager All rights reserved.
+                    </p>
                 </footer>
             </UserDataProvider>
         );
