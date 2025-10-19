d33b35e11544a542215a8f14ec0abcceaa8a7db1
SrKtym <frmywrkplc@gmail.com>
2025-10-19 12:46:26 +0900

    refactor: 関数名の変更

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,15 +3,14 @@ import { NotificationsList } from "@/components/home/notifications-list";
 import { DailySchedule } from "@/components/home/daily-schedule";
 import { UpcomingAssignment } from "@/components/home/upcoming-assignment";
 import { AssignmentProgress } from "@/components/home/assignment-progress";
-import { getSession } from "@/utils/getter";
-import { fetchSchedule } from "@/utils/getter";
+import { fetchSchedule, fetchSession } from "@/utils/getter";
 
 export const metadata: Metadata = {
     title: 'ホーム'
 }
 
 export default async function HomePage() {
-    const session = await getSession();
+    const session = await fetchSession();
     const schedule = await fetchSchedule(session) ?? [];
     const todaySchedule = schedule.filter(
         schedule => schedule.start.toDateString() === new Date().toDateString()
@@ -20,7 +19,7 @@ export default async function HomePage() {
     return (
         <div className="grid grid-cols-1 sm:grid-flow-col grid-rows-3 auto-cols-fr gap-6 p-3">
             <DailySchedule todaySchedule={todaySchedule} />
-            <NotificationsList />
+            <NotificationsList  />
             <UpcomingAssignment />
             <AssignmentProgress />
         </div>
--- app/home/schedule/page.tsx
+++ app/home/schedule/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Overall } from "@/components/home/schedule/overall";
-import { fetchSchedule, getSession } from "@/utils/getter";
+import { fetchSchedule, fetchSession } from "@/utils/getter";
 import { ScheduleProvider } from "@/contexts/schedule-context";
 
 
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 }
 
 export default async function SchedulePage() {
-    const session = await getSession();
+    const session = await fetchSession();
     const schedule = await fetchSchedule(session);
 
     return (
