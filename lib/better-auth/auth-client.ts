de5025b2321ff049bf0fbc07191a1c20d55a3305
SrKtym <frmywrkplc@gmail.com>
2025-12-04 20:34:23 +0900

    fix: 微修正

--- lib/better-auth/auth-client.ts
+++ lib/better-auth/auth-client.ts
@@ -1,8 +1,16 @@
 import { env } from '../../env';
 import { createAuthClient } from 'better-auth/react';
-import { adminClient, passkeyClient } from 'better-auth/client/plugins';
-import { twoFactorClient } from 'better-auth/plugins';
-import { ac, admin, professor, student } from '@/permissions';
+import { 
+    adminClient, 
+    passkeyClient, 
+    twoFactorClient 
+} from 'better-auth/client/plugins';
+import { 
+    ac, 
+    admin, 
+    professor, 
+    student 
+} from '@/permissions';
 
 export const authClient = createAuthClient({
     baseURL: env.NEXT_PUBLIC_APP_URL,
