9d998c486fe9f4ee3c18096ac6d1a666f8b27c8a
SrKtym <frmywrkplc@gmail.com>
2025-12-04 20:24:44 +0900

    fix: 微修正

--- permissions.ts
+++ permissions.ts
@@ -1,27 +1,26 @@
 import { createAccessControl } from "better-auth/plugins/access";
+import { adminAc, defaultStatements } from "better-auth/plugins/admin/access";
 
 const statement = {
+    ...defaultStatements,
     announcement: ["create", "read", "update", "delete"],
-    assignment: ["create", "read", "update", "delete"],
-    user: ["create", "read", "update", "delete", "ban"],  
+    assignment: ["create", "read", "update", "delete"]  
 } as const;
 
 export const ac = createAccessControl(statement);
 
 export const student = ac.newRole({
     announcement: ["read"],
-    assignment: ["read"],
-    user: ["read"],
+    assignment: ["read"]
 });
 
 export const professor = ac.newRole({
     announcement: ["read", "create", "update", "delete"],
     assignment: ["read", "create", "update", "delete"],
-    user: ["read"],
 });
 
 export const admin = ac.newRole({
     announcement: ["create", "read", "update", "delete"],
     assignment: ["create", "read", "update", "delete"],
-    user: ["create", "read", "update", "delete", "ban"],
+    ...adminAc.statements
 });
