eb1a9de64097f07ce2f30bf65509066ea47c5497
SrKtym <frmywrkplc@gmail.com>
2025-12-04 20:27:05 +0900

    feat: リクエストに対するフィルターの追加

--- lib/arcjet/arcjet.ts
+++ lib/arcjet/arcjet.ts
@@ -1,10 +1,14 @@
-import arcjet, { shield } from "@arcjet/next";
+import arcjet, { filter, shield } from "@arcjet/next";
 import { env } from "@/env";
 
 export const aj = arcjet({
     key: env.ARCJET_KEY,
-    characteristics: ["userId"],
+    characteristics: ["ip.src"],
     rules: [
+        filter({
+            deny: ["ip.src.vpn or ip.src.tor"],
+            mode: "LIVE"
+        }),
         shield({
             mode: "LIVE"
         })
