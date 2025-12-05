23dcea3a1e59bd86c1d782534b5061cbf53de114
SrKtym <frmywrkplc@gmail.com>
2025-12-05 15:28:28 +0900

    refactor: 微修正

--- components/home/course-list/course/id/overall.tsx
+++ components/home/course-list/course/id/overall.tsx
@@ -13,29 +13,30 @@ export function Overall() {
     const params = useParams<{id: string}>();
 
     return (
-        <div className="bg-background min-h-screen pb-12 space-y-8">
+        <div className="bg-background pb-12 space-y-8">
             {/* ヘッダー */}
             <AssignmentHeader id={params.id}/>
             
-            <div className="container m-auto px-4 max-w-screen-xl space-y-8">
+            <div className="container m-auto px-4 max-w-screen-xl">
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                    {/* 課題の詳細 */}
+                    <AssignmentDetail id={params.id}/>
 
-                {/* 課題の詳細 */}
-                <AssignmentDetail id={params.id}/>
+                    {/* 提出（学生用） */}
+                    <Submissions id={params.id}/>
 
-                {/* 提出（学生用） */}
-                <Submissions id={params.id}/>
+                    {/* 評定（教員用） */}
+                    <Evaluated />
 
-                {/* 評定（教員用） */}
-                <Evaluated />
+                    {/* コメント */}
+                    <CommentsOnAssignment id={params.id}/>
 
-                {/* コメント */}
-                <CommentsOnAssignment id={params.id}/>
+                    {/* 課題の提出状況 */}
+                    <AssignmentStatus id={params.id}/>
 
-                {/* 課題の提出状況 */}
-                <AssignmentStatus id={params.id}/>
-
-                {/* その他の課題 */}
-                <OtherAssignment id={params.id}/>
+                    {/* その他の課題 */}
+                    <OtherAssignment id={params.id}/>
+                </div>
             </div>
         </div>
     );
