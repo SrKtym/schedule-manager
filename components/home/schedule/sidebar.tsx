61004bc960a5f9e18bad6f61cacdc30c0b97f41f
SrKtym <frmywrkplc@gmail.com>
2025-12-05 13:55:11 +0900

    refactor: スケジュール作成モーダルをモジュール化、ダイナミックインポート化

--- components/home/schedule/sidebar.tsx
+++ components/home/schedule/sidebar.tsx
@@ -1,30 +1,23 @@
 'use client';
 
-import { useContext, useState, useActionState } from "react";
+import { useContext, useState } from "react";
 import {
     getLocalTimeZone, 
     now, 
     today,
     toCalendarDateTime,
 } from "@internationalized/date";
-import { createSchedule } from "@/utils/action";  
 import { 
     Button, 
     Calendar, 
     cn, 
-    DateRangePicker,  
-    Input, 
-    Modal, 
-    ModalBody, 
-    ModalContent, 
-    ModalHeader,
-    Textarea,
     type DateValue,
     type RangeValue
 } from "@heroui/react";
-import { Palette } from "lucide-react";
 import { SidebarContext } from "@/contexts/sidebar-context";
-import { useSessionUserData } from "@/contexts/user-data-context";
+import dynamic from 'next/dynamic';
+
+const CreateScheduleModal = dynamic(() => import('./create-schedule-modal'), { ssr: false });
 
 export function Sidebar({
     setCurrentDate,
@@ -35,7 +28,6 @@ export function Sidebar({
 }) {
     // コンテキストから取得
     const isCollapsed = useContext(SidebarContext);
-    const email = useSessionUserData().email;
 
     // reactフック
     const [open, setOpen] = useState<boolean>(false);
@@ -43,7 +35,6 @@ export function Sidebar({
         start: toCalendarDateTime(now(getLocalTimeZone())),
         end: toCalendarDateTime(now(getLocalTimeZone()))
     });
-    const [state, formAction, isPending] = useActionState(createSchedule, undefined);
     
     return (
         <div className={cn("pt-4 pr-4 z-20 transition-all duration-300",
@@ -66,95 +57,12 @@ export function Sidebar({
                         setSelectedDate(value.toDate(getLocalTimeZone()));
                     }}
                 />
-                <Modal 
-                    backdrop="transparent"
-                    isOpen={open} 
-                    onOpenChange={setOpen}
-                    shouldBlockScroll={false}
-                >
-                    <ModalContent>
-                        <ModalHeader>
-                            予定の追加
-                        </ModalHeader>
-                        <ModalBody>
-                            <form
-                                action={formAction}
-                                className="space-y-5 pb-3"
-                            >
-                                <Input 
-                                    label="タイトルを追加"
-                                    name="title"
-                                    variant="bordered"
-                                />
-                                <DateRangePicker
-                                    aria-label="Date range"
-                                    aria-describedby="dateRange-error"
-                                    label="期間を追加"
-                                    showMonthAndYearPickers={true}
-                                    variant="bordered"
-                                    defaultValue={dateRange}
-                                    onChange={(value) => {
-                                        setDateRange(value);
-                                    }}
-                                />
-                                <input
-                                    name="dateRangeStart"
-                                    type="hidden"
-                                    value={dateRange ? 
-                                        dateRange.start.toDate(getLocalTimeZone()).toISOString() : ''
-                                    }
-                                />
-                                <input
-                                    name="dateRangeEnd"
-                                    type="hidden"
-                                    value={dateRange ? 
-                                        dateRange.end.toDate(getLocalTimeZone()).toISOString() : ''
-                                    }
-                                />
-                                <input
-                                    name="email"
-                                    type="hidden"
-                                    value={email}
-                                />
-                                <div id='dateRange-error' aria-live='polite' aria-atomic='true'>
-                                    {state?.errors?.start && state.errors?.start?.map((error: string) => (
-                                        <p className='text-base text-red-500' key={error}>{error}</p>
-                                    ))}  
-                                </div>
-                                <Textarea 
-                                    label="説明を追加"
-                                    name="description"
-                                    variant="bordered"
-                                />
-                                <Input
-                                    label="カラーを追加"
-                                    labelPlacement="outside-left"
-                                    name="color"
-                                    defaultValue="#000000"
-                                    startContent={<Palette height={35} width={35} />}
-                                    type="color"
-                                    variant="bordered"
-                                />
-                                <div className='flex items-center justify-end space-x-5'>
-                                    <Button
-                                        color="primary"
-                                        type="submit"
-                                        aria-label="Save schedule"
-                                        aria-disabled={isPending}
-                                        isLoading={isPending}
-                                    >
-                                        {isPending ? "保存中..." : "保存"}
-                                    </Button>
-                                    <Button 
-                                        onPress={() => setOpen(prev => !prev)}
-                                    >
-                                        キャンセル
-                                    </Button>
-                                </div>
-                            </form>
-                        </ModalBody>
-                    </ModalContent>
-                </Modal>
+                <CreateScheduleModal
+                    open={open}
+                    setOpen={setOpen}
+                    dateRange={dateRange}
+                    setDateRange={setDateRange}
+                />
             </div>
         </div>
     );
