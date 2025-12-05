5282887bfc8f9450df63eb17cb446d2d2e8e353a
SrKtym <frmywrkplc@gmail.com>
2025-12-05 15:51:36 +0900

    fix: 添付ファイルの削除処理の追加、エラーメッセージの追加

--- components/home/course-list/course/create-announcement-form.tsx
+++ components/home/course-list/course/create-announcement-form.tsx
@@ -13,22 +13,27 @@ import {
 } from '@heroui/react';
 import { useActionState, useState } from 'react';
 import { FileUploader } from './file-uploader';
-import { AttachmentData } from '@/types/regisered-course';
-import { allowedMimeTypes } from '@/constants/definitions';
-import { createAnnouncement } from '@/utils/action';
+import { AttachmentData } from '@/types/main/regisered-course';
+import { allowedMimeTypes, announcementType, attachmentIsRelatedTo } from '@/constants/definitions';
+import { createAnnouncement } from '@/utils/actions/main';
+import { removeFile } from '@/utils/actions/main';
+
 
 export function CreateAnnouncementForm({ courseName }: { courseName: string }) {
-    const [content, setContent] = useState('');
     const [isExpanded, setIsExpanded] = useState(false);
     const [attachments, setAttachments] = useState<AttachmentData[]>([]);
     const [state, formAction, isPending] = useActionState(createAnnouncement, undefined)
     const userData = useSessionUserData();
 
-    const handleFileUpload = (file: AttachmentData) => {
-        setAttachments(prev => [...prev, file]);
-      };
+    // const handleFileUpload = (file: AttachmentData) => {
+    //     setAttachments(prev => [...prev, file]);
+    //   };
     
-    const handleFileRemove = (fileId: string) => {
+    const handleFileRemove = async (
+        fileId: string, 
+        relatedTo: typeof attachmentIsRelatedTo[number]
+    ) => {
+        await removeFile(fileId, relatedTo);
         setAttachments(prev => prev.filter(file => file.id !== fileId));
       };
 
@@ -43,7 +48,7 @@ export function CreateAnnouncementForm({ courseName }: { courseName: string }) {
                     />
                     <div className="flex-1">
                         {isExpanded ? (
-                            <form
+                            <form 
                                 action={formAction}
                                 className='space-y-4'
                             >
@@ -51,40 +56,51 @@ export function CreateAnnouncementForm({ courseName }: { courseName: string }) {
                                     label="タイトル"
                                     name="title"
                                     variant='bordered'
+                                    aria-describedby='title-error'
                                     isRequired
                                 />
+                                <div id='title-error' aria-live='polite' aria-atomic='true'>
+                                    {state?.errors?.title && state.errors.title.map((error: string) => (
+                                        <p className='text-base text-red-500' key={error}>{error}</p>
+                                    ))}
+                                </div>
                                 <Textarea
                                     name="content"
                                     placeholder="アナウンスメントを投稿"
-                                    value={content}
-                                    onValueChange={setContent}
                                     variant='bordered'
                                     minRows={3}
+                                    aria-describedby='content-error'
                                     isRequired
                                 />
+                                <div id='content-error' aria-live='polite' aria-atomic='true'>
+                                    {state?.errors?.content && state.errors.content.map((error: string) => (
+                                        <p className='text-base text-red-500' key={error}>{error}</p>
+                                    ))}
+                                </div>
                                 <Select
                                     label="タイプ"
                                     name="type"
                                     variant='bordered'
+                                    aria-describedby='type-error'
                                     isRequired
                                 >
-                                    <SelectItem>
-                                        資料
-                                    </SelectItem>
-                                    <SelectItem>
-                                        アンケート
-                                    </SelectItem>
-                                    <SelectItem>
-                                        その他
-                                    </SelectItem>
+                                    {announcementType.map((type) => (
+                                        <SelectItem key={type}>
+                                            {type}
+                                        </SelectItem>
+                                    ))}
                                 </Select>
+                                <div id='type-error' aria-live='polite' aria-atomic='true'>
+                                    {state?.errors?.type && state.errors.type.map((error: string) => (
+                                        <p className='text-base text-red-500' key={error}>{error}</p>
+                                    ))}
+                                </div>
                                 <FileUploader 
-                                    onFileUpload={handleFileUpload}
-                                    onFileRemove={handleFileRemove}
                                     files={attachments}
                                     maxFiles={10}
                                     allowedTypes={allowedMimeTypes}
-                                    id={"1"}
+                                    relatedTo="announcement"
+                                    onFileRemove={handleFileRemove}
                                 />
                                 <input
                                     type="hidden"
@@ -95,7 +111,7 @@ export function CreateAnnouncementForm({ courseName }: { courseName: string }) {
                                     <Button 
                                         color="primary"
                                         type="submit"
-                                        isDisabled={!content.trim()}
+                                        aria-disabled={isPending}
                                         isLoading={isPending}
                                     >
                                         投稿
