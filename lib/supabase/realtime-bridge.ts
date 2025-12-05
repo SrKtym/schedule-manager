b7919ea31955848c5428d51e081bc98f7a6aaa00
SrKtym <frmywrkplc@gmail.com>
2025-12-05 13:57:03 +0900

    feat: supabaseのリアルタイム通知を受け取り、メッセージを送信するブリッジ関数を追加

---
+++ lib/supabase/realtime-bridge.ts
@@ -0,0 +1,26 @@
+import { sendMessages } from "@/app/api/[[...route]]/route";
+import { supabaseAdmin } from "./server";
+
+
+function realtimeBridge() {
+    const channel = supabaseAdmin.channel("messages");
+    channel.on("postgres_changes", 
+        { 
+            event: "INSERT",
+            schema: "main",
+            table: "messages"
+        }, 
+        (payload) => {
+            const { subject, body, isread, userId } = payload.new;
+            sendMessages(userId, {
+                subject,
+                body,
+                isread
+            });
+        }
+    );
+
+    channel.subscribe();
+}
+
+realtimeBridge();
