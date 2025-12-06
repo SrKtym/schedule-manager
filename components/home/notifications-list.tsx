e49594d234fbec1f184b637caf246fb5b876a8b6
SrKtym <frmywrkplc@gmail.com>
2025-12-06 13:11:12 +0900

    refactor: 微修正

--- components/home/notifications-list.tsx
+++ components/home/notifications-list.tsx
@@ -11,13 +11,11 @@ import {
 import { useEffect, useState } from "react";
 import { Button } from '@heroui/react';
 import { Bell, Settings, X } from 'lucide-react';
-import { useRegisteredCourseDataList } from '@/contexts/registered-course-context';
 import dynamic from 'next/dynamic';
 
 const SingleItemModal = dynamic(() => import('@/components/home/shared-layout-modal'), { ssr: false });
 
 export function NotificationsList() {
-    const {courseDataList} = useRegisteredCourseDataList();
     const [notifications, setNotifications] = useState([
         { id: 1, title: 'マクロ経済学に新しい課題が追加されました', message: 'これはテスト通知です。通知を消すには右上のXを押してください。この通知リストには、framer-motionを使用してアニメーションを適用しています。', read: false },
         { id: 2, title: 'ミクロ経済学に新しい課題が追加されました', message: 'njkbjhbjhbjkbjkbvjhvkucgcgjugcgcmjcgfhchfchcgfcugjvcgckghchgckhcghcckhchch.', read: false },
