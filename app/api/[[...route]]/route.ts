872892900d82bd4f17ef92b61ad0bbd8cf93b1c7
SrKtym <frmywrkplc@gmail.com>
2025-12-06 12:12:24 +0900

    fix: sendMessages()の引数の型をanyからunknownに修正

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -16,7 +16,7 @@ import { uploadFileSchema } from '@/schemas/form-schema';
 
 const clientsMap = new Map<string, sseClients[]>();
 
-export function sendMessages(userId: string, data: any) {
+export function sendMessages(userId: string, data: unknown) {
     const clients = clientsMap.get(userId) || [];
     clients.forEach(client => client.send(JSON.stringify(data)));
 }
