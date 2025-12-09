f3abee5289fe1aef5498f2ae3bdcfef9f13f4790
SrKtym <frmywrkplc@gmail.com>
2025-12-09 22:02:46 +0900

    fix: 環境変数のファイル分割に伴い一部修正

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
-import { env } from './env';
+import { serverEnv } from './env/server';
 
 export default defineConfig({
     out: './lib/drizzle/migration',
     schema: './lib/drizzle/schemas',
     dialect: 'postgresql',
     dbCredentials: {
-        url: env.DATABASE_URL
+        url: serverEnv.DATABASE_URL
     },
     entities: {
         roles: {
