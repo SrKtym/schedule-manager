c50e4b581c060c9847f60c61c69c25a7b38f76bd
SrKtym <frmywrkplc@gmail.com>
2025-12-09 21:54:00 +0900

    refactor: 環境変数をサーバー用とクライアント用にファイル分割

--- env/server.ts
+++ env/server.ts
@@ -2,7 +2,7 @@ import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod/v4';
 
 
-export const env = createEnv({
+export const serverEnv = createEnv({
     server: {
         BETTER_AUTH_SECRET: z.string(),
         GITHUB_CLIENT_ID: z.string(),
@@ -18,12 +18,6 @@ export const env = createEnv({
         BETTER_AUTH_ADMIN_USER_ID: z.string(),
     },
 
-    client: {
-        NEXT_PUBLIC_APP_URL: z.url(),
-        NEXT_PUBLIC_SUPABASE_URL: z.url(),
-        NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string()
-    },
-
     runtimeEnv: {
         BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET!,
         GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID!,
@@ -32,13 +26,10 @@ export const env = createEnv({
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
         TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID!,
         TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET!,
-        NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL!,
         RESEND_API_KEY: process.env.RESEND_API_KEY!,
         ARCJET_KEY: process.env.ARCJET_KEY!,
         DATABASE_URL: process.env.DATABASE_URL!,
         SUPABASE_SECRET_KEY: process.env.SUPABASE_SECRET_KEY!,
-        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
         BETTER_AUTH_ADMIN_USER_ID: process.env.BETTER_AUTH_ADMIN_USER_ID!
     }
 });
